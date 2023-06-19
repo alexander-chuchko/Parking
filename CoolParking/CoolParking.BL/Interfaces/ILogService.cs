134a23d4f3fa23986a2e14c7227c29524ef6806d
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-06-19 18:33:53 +0300

    Revert "Added project template"
    This reverts commit a1e80dd4b151687b179f2a7103fba4a06d2e2e39.

--- CoolParking/CoolParking.BL/Interfaces/ILogService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoolParking.BL.Interfaces
-{
-    public interface ILogService
-    {
-        string LogPath { get; }
-        void Write(string logInfo);
-        string Read();
-    }
-}
--- CoolParking/CoolParking.BL/Models/Settings.cs
+++
@@ -1,2 +0,0 @@
-﻿// TODO: implement class Settings.
-//       Implementation details are up to you, they just have to meet the requirements of the home task.
