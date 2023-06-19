148a6715f7269c70a37437ce28f893698be90e40
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-06-19 18:55:35 +0300

    Added Vehicle class

--- CoolParking/CoolParking.BL/Models/Vehicle.cs
+++ CoolParking/CoolParking.BL/Models/Vehicle.cs
@@ -5,3 +5,21 @@
 //       The Balance should be able to change only in the CoolParking.BL project.
 //       The type of constructor is shown in the tests and the constructor should have a validation, which also is clear from the tests.
 //       Static method GenerateRandomRegistrationPlateNumber should return a randomly generated unique identifier.
+// TODO: реализовать класс Vehicle.
+// Свойства: Id (строка), VehicleType (ТипТранспорта), Balance (десятичный).
+// Формат идентификатора поясняется в описании домашнего задания.
+// Id и VehicleType не должны изменяться.
+// Баланс должен иметь возможность изменяться только в проекте CoolParking.BL.
+// Тип конструктора показывается в тестах и конструктор должен иметь валидацию, что тоже видно из тестов.
+// Статический метод GenerateRandomRegistrationPlateNumber должен возвращать случайно сгенерированный уникальный идентификатор.
+
+namespace CoolParking.BL.Models
+{
+    public class Vehicle 
+    {
+        public string Id { get; }
+        public VehicleType VehicleType { get; }
+        public decimal Balance { get; internal set; }
+    }
+
+}
