c8fb50fd1336730c4e1575270204008678f9ca86
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-06-19 19:49:12 +0300

    Added VehicleType enum

--- CoolParking/CoolParking.BL/Models/VehicleType.cs
+++ CoolParking/CoolParking.BL/Models/VehicleType.cs
@@ -1,2 +1,13 @@
 ﻿// TODO: implement enum VehicleType.
 //       Items: PassengerCar, Truck, Bus, Motorcycle.
+
+namespace CoolParking.BL.Models
+{
+    enum VehicleType : byte 
+    {
+        PassengerCar,
+        Truck,
+        Bus,
+        Motorcycle
+    }
+}
