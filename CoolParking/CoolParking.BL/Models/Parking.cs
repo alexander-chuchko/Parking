a5c95549b00c08fba550ccc1d51a90af82e93063
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-06-19 23:02:28 +0300

    Added Parking class

--- CoolParking/CoolParking.BL/Models/Parking.cs
+++ CoolParking/CoolParking.BL/Models/Parking.cs
@@ -1,3 +1,25 @@
 ﻿// TODO: implement class Parking.
 //       Implementation details are up to you, they just have to meet the requirements 
 //       of the home task and be consistent with other classes and tests.
+// TODO: реалізувати клас Parking.
+// Деталі впровадження вирішувати вам, вони просто мають відповідати вимогам
+// домашнього завдання та узгоджуватися з іншими класами та тестами.
+
+using System;
+using System.Collections.Generic;
+
+namespace CoolParking.BL.Models
+{
+    public class Parking
+    {
+        public List<Vehicle> Vehicles { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime? StartTime { get; set; }
+
+        private static Parking? instance;
+
+        public Parking()
+        {
+        }
+    }
+}
