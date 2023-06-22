8b91522e8455046f662635c227db912df630a636
Alexander Chuchko <69107323+alexander-chuchko@users.noreply.github.com>
2023-06-23 00:05:14 +0300

    Added DisplayNumberFreeSeats method to the ParkingManagementInterface class

--- CoolParking/CoolParking.BL/ParkingManagementInterface.cs
+++ CoolParking/CoolParking.BL/ParkingManagementInterface.cs
@@ -19,11 +19,21 @@ namespace CoolParking.BL
 
         #region ---helpers---
 
+        //Вивести на екран поточний баланс Паркінгу;
         private void DisplayCurrentBalance()
         {
             Console.WriteLine($"\tParking balance: {_parkingService.GetBalance()}");
         }
 
+        //Вивести на екран кількість вільних місць на Паркінгу (вільно X з Y);
+        private void DisplayNumberFreeSeats()
+        {
+            Console.WriteLine($"\tNumber of free - " +
+                $"{_parkingService.GetFreePlaces()} / employed -" +
+                $" {_parkingService.GetCapacity() - _parkingService.GetFreePlaces()}");
+        }
+
+        //Вивести на екран суму зароблених коштів за поточний період(до запису у лог);
         private void DisplayEarnings()
         {
             var transactionsLog = _parkingService.GetLastParkingTransactions();
@@ -38,6 +48,31 @@ namespace CoolParking.BL
             }
         }
 
+        private void ShowListTrFundsLocated()
+        {
+            if (_parkingService.GetFreePlaces() < Settings.parkingCapacity)
+            {
+                int count = default(int);
+                Console.WriteLine($"\tVehicle list:\n");
+
+                foreach (var item in _parkingService.GetVehicles())
+                {
+                    Console.WriteLine($"\t{++count} - Id:{item.Id} VehicleType:{item.VehicleType} Balance:{item.Balance}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\tThere are no cars in the parking lot");
+            }
+        }
+        //DisplayAllParkingTrCurrentPeriod
+
+        private void PutTrAidForParking()
+        {
+            var vehicle = new Vehicle(Vehicle.GenerateRandomRegistrationPlateNumber(), VehicleType.Truck, 100);
+            _parkingService.AddVehicle(vehicle);
+            Console.WriteLine($"\tAdded to the parking car - Id:{vehicle.Id} VehicleType:{vehicle.VehicleType} Balance:{vehicle.Balance}");
+        }
 
         #endregion
     }
